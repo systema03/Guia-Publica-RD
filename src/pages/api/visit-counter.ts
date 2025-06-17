c1a4d1930bd270884a241becc50828d12cd7df0b
systema03 <antuandlc01@gmail.com>
2025-06-16 20:54:44 -0400

    Update visit-counter.ts

--- src/pages/api/visit-counter.ts
+++ src/pages/api/visit-counter.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'express'
 import { redis } from '@/lib/upstash'
 
 export default async function handler(
